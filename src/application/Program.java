884efaee2e24653f68853c576cf6f99d7e072da8
Giuliano F Pezzi <gfpezzi@gmail.com>
2020-05-06 00:27:40 -0300

    Melhorias da classe Program e classe Cronometro

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,10 +9,43 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		Cronometro cr = new Cronometro();
-
-			cr.contador();
-
+		
+		int paraHora = 0;
+		int paraMinuto = 0;
+		int paraSegundo = 0;
+		boolean formatoTempoCorreto = false;
+		
+		do {
+			System.out.print("Configure o tempo (xx:xx:xx): ");
+			String tempo = sc.next();
+			try {
+				paraHora = Integer.parseInt(tempo.substring(0,2));
+				paraMinuto = Integer.parseInt(tempo.substring(3,5));
+				paraSegundo = Integer.parseInt(tempo.substring(6,8));
+				formatoTempoCorreto = true;
+			} catch(Exception e)
+			{
+				System.out.println("Formato errado de entrada!");
+				System.out.println("Exemplo de entrada de dados: 01:45:50, 00:02:30, 00:00:40, 00:85:00, 00:00:80");
+				System.out.println();
+			}
+			
+			}while(!formatoTempoCorreto);
+		
+			if(paraSegundo >= 60) {
+				paraMinuto = paraSegundo/60;
+				paraSegundo = paraSegundo -60; 
+			}
+			
+			if(paraMinuto >= 60) {
+				paraHora = paraMinuto/60;
+				paraMinuto = paraMinuto -60; 
+			}
+		
+			Cronometro cr = new Cronometro();
+			cr.paraCronometro(paraHora, paraMinuto, paraSegundo);
+			cr.cronometro();
+			
 		sc.close();
 	}
 
--- src/entities/Cronometro.java
+++ src/entities/Cronometro.java
@@ -2,9 +2,13 @@ package entities;
 
 public class Cronometro {
 	
-	private int segundo;
+	private int segundo = 1;
+	private int paraSegundo;
 	private int minuto;
+	private int paraMinuto;
 	private int hora;
+	private int paraHora;
+	private boolean para = false;
 	
 	public Cronometro() {
 	}
@@ -17,12 +21,28 @@ public class Cronometro {
 		this.segundo = segundo;
 	}
 	
+	public int getParaSegundo() {
+		return paraSegundo;
+	}
+
+	public void setParaSegundo(int paraSegundo) {
+		this.paraSegundo = paraSegundo;
+	}
+	
 	public int getMinuto() {
 		return minuto;
 	}
 
 	public void setMinuto(int minuto) {
 		this.minuto = minuto;
+	}
+	
+	public int getParaMinuto() {
+		return paraMinuto;
+	}
+
+	public void setParaMinuto(int paraMinuto) {
+		this.paraMinuto = paraMinuto;
 	}
 
 	public int getHora() {
@@ -33,10 +53,39 @@ public class Cronometro {
 		this.hora = hora;
 	}
 	
-	public void contador() {
-		while(hora<60) {
-			while(minuto<60) {
-				while(segundo<60) {
+	public int getParaHora() {
+		return paraHora;
+	}
+
+	public void setParaHora(int paraHora) {
+		this.paraHora = paraHora;
+	}
+
+	public boolean getPara() {
+		return para;
+	}
+
+	public void setPara(boolean para) {
+		this.para = para;
+	}
+	
+	public void paraCronometro(int paraHora, int paraMinuto, int paraSegundo) {
+		this.paraHora = paraHora;
+		this.paraMinuto = paraMinuto;
+		this.paraSegundo = paraSegundo;
+	}
+
+	public void cronometro() {
+		System.out.println("00:00:00");
+		while(hora<60 && !para) {
+			while(minuto<60 && !para) {
+				while(segundo<60 && !para) {
+					if(paraHora == hora && paraMinuto == minuto && paraSegundo == segundo) {
+						para = true;
+					}
+					else {
+						para = false;
+					}
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -64,7 +113,7 @@ public class Cronometro {
 					}
 					segundo++;
 				}
-				segundo =0;
+				segundo = 0;
 				minuto++;
 			}
 			minuto = 0;
